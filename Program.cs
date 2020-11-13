d6562c95a2f387cae2524d66ad560a67d3a384be
DEV <dev@info.local>
2020-11-13 10:04:54 -0300

    Exercicio 1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Primeiro_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // entrada de dados
+            Console.WriteLine("Qual é a sua idade");
+            int idade = int.Parse(Console.ReadLine());
+        // processamento
+            int idademes = idade * 12;
+            int idadedia = idade * 365;
+            int idadehora = idade * 8760;
+
+        //exibir 
+            Console.WriteLine("Sua idade em mês é "+idademes+" Sua idade em dias é "+idadedia+ " e Sua idade em horas é "+idadehora);
+        }
+    }
+}
